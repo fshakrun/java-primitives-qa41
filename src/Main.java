a80ded2e81f55895ef4fe2f71c713a66b5f6cc02
Fedor Shakrun <fshakrun@gmail.com>
2022-07-11 13:34:51 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int tiket = 1000;
+        int bonus;
+        bonus = tiket/20;
+        System.out.println(bonus);
+
+    }
+}
